b2b4774f3feb2f260c8b6784552ed9d4e9536e24
willylsilva <49284385+willylsilva@users.noreply.github.com>
2023-01-07 01:11:41 +0000

    test

--- demo/src/main/java/com/example/mongotest/demo/adapter/in/TestController.java
+++ demo/src/main/java/com/example/mongotest/demo/adapter/in/TestController.java
@@ -1,5 +1,7 @@
 package com.example.mongotest.demo.adapter.in;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class TestController {
 
+    private final Logger log = LogManager.getLogger(TestController.class);
+
     @PostMapping
     public String execute() {
+        log.info("Has a bug on the Enter button");
         return "Look at this beatufil workaround JJ\n";
     }
 }
